29f69a50b90f8f8890b0478bfb46dad8711c584e
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-23 19:03:29 +0900

    WIP

--- main.sh
+++ main.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# 操作対象とするファイルのリストを取得
+dir_path="${1}*"
+target_files=`find $dir_path -type f`
+
+# 変更後のファイル名
+# 直近のディレクトリまでのパスのみを切り出し、2つ名の変数で指定されたファイル名を連結する
+changed_name="${2}"
+
+# 各ファイル名を置換
+for file in $target_files;
+do
+	echo $file
+	mv $file 
+done
