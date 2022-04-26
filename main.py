56ae98091d47797e509905ef89e1a1ffb034f357
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-26 18:16:47 +0900

    removed unnecessary files and codes

--- main.py
+++ main.py
@@ -3,27 +3,23 @@ import sys
 import glob
 
 def main():
-    # 標準入力から対象ディレクトリ名と変換後ファイル名を取得
-    target_dir, conv_file_name = sys.argv[1], sys.argv[2]
-    print(f"target directory: {target_dir}, converted file name: {conv_file_name}")
+    target_dir = sys.argv[1]
+    print(f"target directory: {target_dir}")
 
-    # コマンドライン引数が不足していた場合のエラーハンドリング
-    if(target_dir == None or conv_file_name == None):
-        pass
+    print(f"Are you sure to rename all files in this directory into {target_dir}?: [Y/n]")
+    intention = input()
+    if intention == "n":
+        return
+    elif intention == "Y":
+        if(target_dir): # コマンドライン引数が不足していた場合のエラーハンドリング
+            pass
+        target_files = glob.glob(f"./{target_dir}/*")
 
-    # 対象ディレクトリ内のファイル名を取得
-    target_files = glob.glob(f"./{target_dir}/*")
-    print(target_files)
-
-    # ディレクトリを除く
-
-    # 所望の拡張子を持つファイル以外が混入していないかを検閲
-    # desired_extensions = ["png", "jpg", "JEPG", "HEIC"]
-    # checked_files = rm_undesired_files(target_files, desired_extensions)
-
-    # 変換後ファイル名と識別番号を組み合わせたファイル名で対象ファイル名を上書き
-    for id, target_file_path in enumerate(target_files):
-        rename_with_id(target_file_path, conv_file_name, str(id+1))
+        # 変換後ファイル名と識別番号を組み合わせたファイル名で対象ファイル名を上書き
+        for id, target_file_path in enumerate(target_files):
+            rename_with_id(target_file_path, target_dir, str(id+1))
+    else:
+        print("Please input Y or n.")
 
 def rm_undesired_files(file_list, extensions):
     """
@@ -32,6 +28,7 @@ def rm_undesired_files(file_list, extensions):
     :return 所望の拡張子を持つファイルのみのリスト
     """
     for file in file_list:
+        print(file)
         for ext in extensions:
             if not file.endswith(ext):
                 file_list.remove(file)
