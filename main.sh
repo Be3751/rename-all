fffdb8de59060ba5924963c84e21d6dfcb148447
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-04-26 09:56:18 +0900

    WIP

--- main.sh
+++ main.sh
@@ -6,11 +6,13 @@ target_files=`find $dir_path -type f`
 
 # 変更後のファイル名
 # 直近のディレクトリまでのパスのみを切り出し、2つ名の変数で指定されたファイル名を連結する
-changed_name="${2}"
+# target/hogehoge.png -> target/
+changed_name="${1%/*.}"
+echo $changed_name
 
 # 各ファイル名を置換
 for file in $target_files;
 do
 	echo $file
-	mv $file 
+	# mv $file 
 done
